0894299fd4d5e48e20236d4a319387ecb1e8b778
Jurandi Junior <jurandijr20@gmail.com>
2025-02-18 19:08:46 -0300

    Restrições de generics

--- Program.cs
+++ Program.cs
@@ -1,9 +1,31 @@
-﻿namespace POO_CSharp_20
+﻿using POO_CSharp_P20.Entities;
+using POO_CSharp_P20.Services;
+using System.Globalization;
+
+namespace POO_CSharp_20
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products:  ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(vect[0], price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product p = calculationService.Max(list); // <Product> is optional
+
+            Console.WriteLine("Most expensive: ");
+            Console.WriteLine(p);
         }
     }
 }
